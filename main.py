07746de5a16252ef791d27efcbcaac27b3c5aaff
Julian Nunez <julian.nunezm@gmail.com>
2026-06-06 11:52:19 +1000

    Refactor main by encapsulating all the db-related logic to its package.

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
 from common import db_functions as db
 
 if __name__ == '__main__':
-    if not db.validate_db_tables():
-        db.create_tables()
-        db.insert_main_records()
-
-    db.get_accounts()
-    db.get_statement_periods()
+    db.execute_db_functions()
